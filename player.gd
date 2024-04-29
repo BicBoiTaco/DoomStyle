86257675de435ce341f704bf3468047dc4e79b6f
Devoodie <devantecoley802@gmail.com>
2024-04-29 12:06:01 -0400

    Unfucked the git directory!

--- player.gd
+++ player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
 
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 4.5
+@export var speed = 5.0
+@export var sprintspeed = 10.0
+@export var jump_velocity = 4.5
+@export_range(0.001, 0.01) var mouse_sens = 0.01
 
 @onready var neck := $neck
 @onready var fpscamera := $neck/fpscamera
@@ -15,8 +17,8 @@ func _unhandled_input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x * 0.01)
-			fpscamera.rotate_x(-event.relative.y * 0.01)
+			neck.rotate_y(-event.relative.x * mouse_sens)
+			fpscamera.rotate_x(-event.relative.y * mouse_sens)
 			fpscamera.rotation.x =clamp(fpscamera.rotation.x, deg_to_rad(-75), deg_to_rad(75))
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -29,17 +31,22 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("back", "forward", "left", "right")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		
+	if Input.is_action_pressed("sprint"):
+		speed = sprintspeed
+	else:
+		speed = 5
 
 	move_and_slide()
